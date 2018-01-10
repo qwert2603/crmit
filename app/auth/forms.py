9745587656d217475a1991dbd7430f8c1506e7bf
alex <qwert2603@mail.ru>
2018-01-10 09:33:29 +0700

    * forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,29 +1,108 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
-from wtforms.validators import Email, Length, DataRequired, EqualTo, Regexp
-from app.models import SystemUser
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError, DateField, SelectField, \
+    IntegerField
+from wtforms.validators import Length, DataRequired, EqualTo, Regexp, Optional, Email
+
+from app.models import SystemUser, School, Citizenship, Parent, Section, Group
 
 
 class LoginForm(FlaskForm):
-    login = StringField('логин или email', validators=[DataRequired(), Length(1, 64)])
+    login = StringField('логин или email', validators=[Length(1, 64)])
     password = PasswordField('пароль', validators=[DataRequired()])
     remember_me = BooleanField('запомнить меня')
     submit = SubmitField('войти')
 
 
+# base field for registration.
 class RegistrationForm(FlaskForm):
-    username = StringField('username', validators=[
-        DataRequired(), Length(1, 64), Regexp('^[a-z][a-z0-9_]*$', 0, 'only letters, digits and underscores')])
-    email = StringField('email', validators=[DataRequired(), Length(5, 64), Email()])
-    password = PasswordField('password', validators=[DataRequired()])
-    password_confirm = PasswordField('confirm password', validators=[
-        DataRequired(), EqualTo('password', 'passwords must match')])
-    submit = SubmitField('register')
+    login = StringField('логин', validators=[Length(1, 64),
+                                             Regexp('^[a-z][a-z0-9_]*$', 0, 'только буквы / цифры / подчеркивание')])
+    fio = StringField('фио', validators=[Length(1, 255), Regexp('^[а-яА-Я]*$', 0, 'только русские буквы')])
+    password = PasswordField('пароль', validators=[Length(1, 255)])
+    password_confirm = PasswordField('подтверждение пароля', validators=[
+        DataRequired(), EqualTo('password', 'пароли должны совпадать')])
 
-    def validate_email(self, field):
-        if SystemUser.query.filter_by(email=field.data).first():
-            raise ValidationError('email already registered!')
+    def validate_login(self, field):
+        if SystemUser.query.filter_by(login=field.data).first():
+            raise ValidationError('логин уже Занят!')
 
-    def validate_username(self, field):
-        if SystemUser.query.filter_by(username=field.data).first():
-            raise ValidationError('username already registered!')
+
+class RegistrationMasterForm(RegistrationForm):
+    submit = SubmitField('создать мастера')
+
+
+class RegistrationTeacherForm(RegistrationForm):
+    submit = SubmitField('создать преподавателя')
+
+
+class RegistrationStudentForm(RegistrationForm):
+    birth_date = DateField('дата рождения', validators=[DataRequired()])
+    birth_place = StringField('место рождения', validators=[Length(1, 255)])
+    registration_place = StringField('адрес регистрации', validators=[Length(1, 255)])
+    actual_address = StringField('фактический адрес проживания', validators=[Length(1, 255)])
+    additional_info = StringField('дополнительная информация', validators=[Length(0, 255)])
+    known_from = StringField('откуда узнал(а) о ЦМИТ', validators=[Length(0, 255)])
+    school = SelectField('школа', coerce=int, validators=[DataRequired()])
+    citizenship = SelectField('гражданство', coerce=int, validators=[DataRequired()])
+    mother = SelectField('мать', coerce=int, validators=[Optional()])
+    father = SelectField('отец', coerce=int, validators=[Optional()])
+    submit = SubmitField('создать ученика')
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationStudentForm, self).__init__(*args, *kwargs)
+        self.school.choices = [(school.id, school.name) for school in School.query.order_by(School.name).all()]
+        self.citizenship.choices = [(c.id, c.name) for c in Citizenship.query.order_by(School.name).all()]
+
+
+class ParentForm(FlaskForm):
+    fio = StringField('фио', validators=[Length(1, 255), Regexp('^[а-яА-Я]*$', 0, 'только русские буквы')])
+    phone = StringField('телефон', validators=[Length(1, 32)])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    passport = StringField('паспорт', validators=[Length(1, 255)])
+    address = StringField('адрес', validators=[Length(1, 255)])
+    home_phone = StringField('домашний телефон', validators=[Length(0, 32)])
+
+    def validate_passport(self, field):
+        if Parent.query.filter_by(passport=field.data).first():
+            raise ValidationError('этот паспорт уже зарегистрирован!')
+
+
+class SchoolForm(FlaskForm):
+    name = StringField('название школы', validators=[Length(1, 255)])
+
+    def validate_name(self, field):
+        if School.query.filter_by(name=field.data).first():
+            raise ValidationError('школа с Таким названием уже существует!')
+
+
+class CitizenshipForm(FlaskForm):
+    name = StringField('гражданство', validators=[Length(1, 255)])
+
+    def validate_name(self, field):
+        if Citizenship.query.filter_by(name=field.data).first():
+            raise ValidationError('гражданство с Таким названием уже существует!')
+
+
+class SectionForm(FlaskForm):
+    name = StringField('название секции', validators=[Length(1, 255)])
+    price = IntegerField('цена за месяц', validators=[DataRequired()])
+
+    def validate_name(self, field):
+        if Section.query.filter_by(name=field.data).first():
+            raise ValidationError('секция с Таким названием уже существует!')
+
+    def validate_price(self, field):
+        if field.data <= 0:
+            raise ValidationError('цена должна быть больше 0!')
+        if field.data > 10000:
+            raise ValidationError('цена слишком БОЛЬШАЯ!')
+
+
+class GroupForm(FlaskForm):
+    section = SelectField('секция', validators=[DataRequired()])
+    name = StringField('название', validators=[Length(1, 255)])
+    teacher = SelectField('преподаватель', validators=[DataRequired()])
+
+    def validate_name(self, field):
+        if Group.query.filter_by(name=field.data).first():
+            raise ValidationError('группа с Таким названием уже существует!')
--- app/decorators.py
+++ app/decorators.py
@@ -4,11 +4,11 @@ from flask_login import current_user
 from app.init_model import role_master_name, role_teacher_name
 
 
-def check_system_role(system_role_name):
+def check_system_role(system_role_names):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            if not current_user.system_role.name == system_role_name:
+            if not current_user.system_role.name in system_role_names:
                 abort(403)
             return f(*args, **kwargs)
 
@@ -18,8 +18,12 @@ def check_system_role(system_role_name):
 
 
 def check_master(f):
-    return check_system_role(role_master_name)(f)
+    return check_system_role([role_master_name])(f)
 
 
 def check_teacher(f):
-    return check_system_role(role_teacher_name)(f)
+    return check_system_role([role_teacher_name])(f)
+
+
+def check_master_or_teacher(f):
+    return check_system_role([role_master_name, role_teacher_name])(f)
