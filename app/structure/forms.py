6ad4c409825f69a2b56a1faa74eeda0cef078bbd
alex <qwert2603@mail.ru>
2018-01-10 20:32:51 +0700

    * edit forms in structure_blueprint

--- app/structure/forms.py
+++ app/structure/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, ValidationError, IntegerField, SelectField, SubmitField
+from wtforms import StringField, ValidationError, IntegerField, SelectField, SubmitField, Label
 from wtforms.validators import Length, DataRequired, Email, Regexp
 from app.models import Group, Section, Citizenship, School, Parent, Teacher
 
@@ -13,8 +13,15 @@ class ParentForm(FlaskForm):
     home_phone = StringField('домашний телефон', validators=[Length(0, 32)])
     submit = SubmitField('создать')
 
+    def __init__(self, parent=None, *args, **kwargs):
+        super(ParentForm, self).__init__(*args, **kwargs)
+        self.parent = parent
+        if parent is not None:
+            self.submit.label = Label(self.submit.id, 'сохранить')
+
     def validate_passport(self, field):
-        if Parent.query.filter_by(passport=field.data).first():
+        if (self.parent is None or self.passport.data != self.parent.passport) \
+                and Parent.query.filter_by(passport=field.data).first():
             raise ValidationError('этот паспорт уже зарегистрирован!')
 
 
@@ -22,8 +29,15 @@ class SchoolForm(FlaskForm):
     name = StringField('название школы', validators=[Length(1, 255)])
     submit = SubmitField('создать')
 
+    def __init__(self, school=None, *args, **kwargs):
+        super(SchoolForm, self).__init__(*args, **kwargs)
+        self.school = school
+        if school is not None:
+            self.submit.label = Label(self.submit.id, 'сохранить')
+
     def validate_name(self, field):
-        if School.query.filter_by(name=field.data).first():
+        if (self.school is None or self.name.data != self.school.name) \
+                and School.query.filter_by(name=field.data).first():
             raise ValidationError('школа с Таким названием уже существует!')
 
 
@@ -31,8 +45,15 @@ class CitizenshipForm(FlaskForm):
     name = StringField('гражданство', validators=[Length(1, 255)])
     submit = SubmitField('создать')
 
+    def __init__(self, citizenship=None, *args, **kwargs):
+        super(CitizenshipForm, self).__init__(*args, **kwargs)
+        self.citizenship = citizenship
+        if citizenship is not None:
+            self.submit.label = Label(self.submit.id, 'сохранить')
+
     def validate_name(self, field):
-        if Citizenship.query.filter_by(name=field.data).first():
+        if (self.citizenship is None or field.data != self.citizenship.name) \
+                and Citizenship.query.filter_by(name=field.data).first():
             raise ValidationError('гражданство с Таким названием уже существует!')
 
 
@@ -41,8 +62,15 @@ class SectionForm(FlaskForm):
     price = IntegerField('цена за месяц', validators=[DataRequired()])
     submit = SubmitField('создать')
 
+    def __init__(self, section=None, *args, **kwargs):
+        super(SectionForm, self).__init__(*args, **kwargs)
+        self.section = section
+        if section is not None:
+            self.submit.label = Label(self.submit.id, 'сохранить')
+
     def validate_name(self, field):
-        if Section.query.filter_by(name=field.data).first():
+        if (self.section is None or self.name.data != self.section.name) \
+                and Section.query.filter_by(name=field.data).first():
             raise ValidationError('секция с Таким названием уже существует!')
 
     def validate_price(self, field):
@@ -58,11 +86,15 @@ class GroupForm(FlaskForm):
     teacher = SelectField('преподаватель', coerce=int, validators=[DataRequired()])
     submit = SubmitField('создать')
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, group=None, *args, **kwargs):
         super(GroupForm, self).__init__(*args, *kwargs)
         self.section.choices = [(section.id, section.name) for section in Section.query.order_by(Section.name).all()]
         self.teacher.choices = [(teacher.id, teacher.fio) for teacher in Teacher.query.order_by(Teacher.fio).all()]
+        self.group = group
+        if group is not None:
+            self.submit.label = Label(self.submit.id, 'сохранить')
 
     def validate_name(self, field):
-        if Group.query.filter_by(name=field.data).first():
+        if (self.group is None or self.name.data != self.group.name) \
+                and Group.query.filter_by(name=field.data).first():
             raise ValidationError('группа с Таким названием уже существует!')
---
+++ app/structure/views_edit.py
@@ -0,0 +1,92 @@
+from flask import render_template, redirect, url_for, flash
+
+from app.decorators import check_master
+from app.models import Citizenship, Section, Parent, School, Group
+from app.structure import structure
+from app.structure.forms import CitizenshipForm, SectionForm, ParentForm, SchoolForm, GroupForm
+
+
+@structure.route('/section/<int:id>', methods=['GET', 'POST'])
+@check_master
+def edit_section(id):
+    section = Section.query.get_or_404(id)
+    form = SectionForm(section)
+    if form.validate_on_submit():
+        section.name = form.name.data
+        section.price = form.price.data
+        flash('секция {} изменена'.format(form.name.data))
+        return redirect(url_for('main.index'))
+    if not form.is_submitted():
+        form.name.data = section.name
+        form.price.data = section.price
+    return render_template('structure/form_edit.html', form=form, class_name='секции')
+
+
+@structure.route('/citizenship/<int:id>', methods=['GET', 'POST'])
+@check_master
+def edit_citizenship(id):
+    citizenship = Citizenship.query.get_or_404(id)
+    form = CitizenshipForm(citizenship)
+    if form.validate_on_submit():
+        citizenship.name = form.name.data
+        flash('гражданство {} изменено'.format(form.name.data))
+        return redirect(url_for('main.index'))
+    if not form.is_submitted():
+        form.name.data = citizenship.name
+    return render_template('structure/form_edit.html', form=form, class_name='гражданства')
+
+
+@structure.route('/school/<int:id>', methods=['GET', 'POST'])
+@check_master
+def edit_school(id):
+    school = School.query.get_or_404(id)
+    form = SchoolForm(school)
+    if form.validate_on_submit():
+        school.name = form.name.data
+        flash('школа {} изменена'.format(form.name.data))
+        return redirect(url_for('main.index'))
+    if not form.is_submitted():
+        form.name.data = school.name
+    return render_template('structure/form_edit.html', form=form, class_name='школы')
+
+
+@structure.route('/parent/<int:id>', methods=['GET', 'POST'])
+@check_master
+def edit_parent(id):
+    parent = Parent.query.get_or_404(id)
+    form = ParentForm(parent)
+    if form.validate_on_submit():
+        parent.fio = form.fio.data
+        parent.phone = form.phone.data
+        parent.email = form.email.data
+        parent.passport = form.passport.data
+        parent.address = form.address.data
+        parent.home_phone = form.home_phone.data
+        flash('родитель {} изменен'.format(form.fio.data))
+        return redirect(url_for('main.index'))
+    if not form.is_submitted():
+        form.fio.data = parent.fio
+        form.phone.data = parent.phone
+        form.email.data = parent.email
+        form.passport.data = parent.passport
+        form.address.data = parent.address
+        form.home_phone.data = parent.home_phone
+    return render_template('structure/form_edit.html', form=form, class_name='родителя')
+
+
+@structure.route('/group/<int:id>', methods=['GET', 'POST'])
+@check_master
+def edit_group(id):
+    group = Group.query.get_or_404(id)
+    form = GroupForm(group)
+    if form.validate_on_submit():
+        group.name = form.name.data
+        group.teacher_id = form.teacher.data
+        group.section_id = form.section.data
+        flash('группа {} изменена'.format(form.name.data))
+        return redirect(url_for('main.index'))
+    if not form.is_submitted():
+        form.name.data = group.name
+        form.teacher.data = group.teacher_id
+        form.section.data = group.section_id
+    return render_template('structure/form_edit.html', form=form, class_name='группы')
