146530f0ec2f29ea465f57c06cf49d65d5880206
alex <qwert2603@mail.ru>
2018-02-16 22:21:00 +0700

    \

--- app/structure/views.py
+++ app/structure/views.py
@@ -20,8 +20,6 @@ def students_in_group(id):
     in_group_students_ids = [s.student.id for s in students_in_group]
     if 'submit' in request.form:
         form_in_group = [int(i) for i in request.form.getlist('in_group')]
-        print(in_group_students_ids)
-        print(form_in_group)
         for new_id in form_in_group:
             if new_id not in in_group_students_ids:
                 db.session.add(StudentInGroup(student_id=new_id, group_id=id, enter_month=group.start_month,
