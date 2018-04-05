4dfbbf5c33aa5cc33a57d6467df233f463e858a1
alex <qwert2603@mail.ru>
2018-04-05 20:28:48 +0700

    * delete teachers / students

--- app/is_removable_check.py
+++ app/is_removable_check.py
@@ -16,10 +16,12 @@ def is_parent_removable(parent): return parent.parent_of_students.count() == 0
 def is_group_removable(group): return group.students_in_group.count() == 0 and group.lessons.count() == 0
 
 
-def is_student_removable(student): pass
-
-
-def is_teacher_removable(teacher): pass
+def is_teacher_removable(teacher): return teacher.groups.count() == 0 and teacher.lessons.count() == 0
 
 
 def is_master_removable(master): return master.system_user_id != current_user.id
+
+
+def is_student_removable(student): return student.parent_of_students.count() == 0 \
+                                          and student.students_in_groups.count() == 0 \
+                                          and student.attendings.count() == 0
--- app/users/views_delete.py
+++ app/users/views_delete.py
@@ -1,10 +1,10 @@
 from flask import flash, redirect, url_for, abort
 from flask_login import login_required
 from app import db
-from app.is_removable_check import is_master_removable
-from app.models import Master
+from app.is_removable_check import is_master_removable, is_teacher_removable, is_student_removable
+from app.models import Master, Teacher, Student
 from app.users import users
-from app.decorators import check_master
+from app.decorators import check_master, check_master_or_teacher
 
 
 @users.route('/delete_master/<int:id>')
@@ -17,3 +17,27 @@ def delete_master(id):
     db.session.delete(master.system_user)
     flash('руководитель удалён')
     return redirect(url_for('.masters_list'))
+
+
+@users.route('/delete_teacher/<int:id>')
+@login_required
+@check_master
+def delete_teacher(id):
+    teacher = Teacher.query.get_or_404(id)
+    if not is_teacher_removable(teacher): abort(409)
+    db.session.delete(teacher)
+    db.session.delete(teacher.system_user)
+    flash('преподаватель удалён')
+    return redirect(url_for('.teachers_list'))
+
+
+@users.route('/delete_student/<int:id>')
+@login_required
+@check_master_or_teacher
+def delete_student(id):
+    student = Student.query.get_or_404(id)
+    if not is_student_removable(student): abort(409)
+    db.session.delete(student)
+    db.session.delete(student.system_user)
+    flash('ученик удалён')
+    return redirect(url_for('.students_list'))
