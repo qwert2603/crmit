21f3a87d33473973b8f2104eecbcdd813b1f062e
alex <qwert2603@mail.ru>
2018-05-21 12:59:28 +0700

    \

---
+++ app/vk.py
@@ -0,0 +1,10 @@
+def send_vk_message(subject, body, recipients):
+    # todo
+    return
+
+
+def send_test_vk_message():
+    # todo: recipients
+    send_vk_message('test subject', body='text body', recipients=['qwert2603'])
+
+# from app.vk import send_test_vk_message
