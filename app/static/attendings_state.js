6a53988493532f33e8d99e9b992daccb2a0ef7a9
alex <qwert2603@mail.ru>
2018-07-01 17:26:35 +0700

    * attendings state switcher

---
+++ app/static/attendings_state.js
@@ -0,0 +1,31 @@
+function makeStateSwitcher(stateContainer) {
+    const radios = stateContainer.getElementsByTagName('input');
+    const stateTexts = stateContainer.getElementsByClassName('state_text');
+
+    let state = 0;
+    for (let i = 0; i < radios.length; ++i) {
+        if (radios[i].checked) {
+            state = i;
+            break;
+        }
+    }
+
+    function render(state) {
+        radios[state].checked = true;
+        for (let i = 0; i < stateTexts.length; ++i) {
+            stateTexts[i].style.display = "none";
+        }
+        stateTexts[state].style.display = "";
+    }
+
+    stateContainer.onclick = function () {
+        state = (state + 1) % radios.length;
+        render(state)
+    };
+    render(state);
+}
+
+const stateContainers = document.getElementsByClassName('state_container');
+for (let i = 0; i < stateContainers.length; ++i) {
+    makeStateSwitcher(stateContainers[i]);
+}
