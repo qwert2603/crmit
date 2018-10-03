47f04ff11f62160938c73ee2151d2c2b98d70748
alex <qwert2603@mail.ru>
2018-10-03 20:31:16 +0700

    \

--- app/api_1_0/__init__.py
+++ app/api_1_0/__init__.py
@@ -8,9 +8,15 @@ api_1_0 = Blueprint('api_1_0', __name__)
 from . import rests, errors
 
 
+@api_1_0.before_request
+def before_request():
+    g.current_user_app = None
+
+
 @api_1_0.after_request
 def after_request(response):
-    if g.current_user_app.is_authenticated and g.current_user_app.login == developer_login:
+    current_user_app = g.current_user_app
+    if current_user_app is not None and current_user_app.is_authenticated and current_user_app.login == developer_login:
         from manage import app
         if app.config['DEVELOPER_READ_ONLY']:
             db.session.rollback()
