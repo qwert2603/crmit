c4d6946674205593ad887c0f3867aa38023aee02
alex <qwert2603@mail.ru>
2018-10-16 15:02:52 +0700

    * anth section

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,6 @@
+from flask_login import login_required
+
+from app.decorators import check_master
 from app.main import main
 from flask import render_template
 
@@ -5,3 +8,17 @@ from flask import render_template
 @main.route('/')
 def index():
     return render_template('index.html', hide_who_u_r=True)
+
+
+@main.route('/anth')
+@login_required
+@check_master
+def anth():
+    return render_template('anth.html')
+
+
+@main.route('/dump')
+@login_required
+@check_master
+def dump():
+    return '// todo'
