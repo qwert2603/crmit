0831a5566277ac0495971ae98f8459a700eafa31
alex <qwert2603@mail.ru>
2019-03-04 14:26:00 +0700

    select teacher in groups list

--- app/structure/views_list.py
+++ app/structure/views_list.py
@@ -3,7 +3,7 @@ from flask_login import login_required
 
 from app.decorators import check_master_or_teacher, check_master
 from app.list_route import create_list_route
-from app.models import Citizenship, Section, Parent, School, Group
+from app.models import Citizenship, Section, Parent, School, Group, Teacher
 from app.structure import structure
 
 
@@ -21,9 +21,22 @@ def citizenships_list():
 @login_required
 @check_master_or_teacher
 def groups_list():
-    return create_list_route(
-        lambda search: Group.list_sorted_for_current_user().filter(Group.name.ilike('%{}%'.format(search))),
-        'structure/groups_list.html')
+    teacher_id = request.args.get('teacher_id', 0, type=int)
+    search = request.args.get('search', '')
+    page = request.args.get('page', 1, type=int)
+    pagination = Group.list_sorted_for_current_user() \
+        .filter(Group.name.ilike('%{}%'.format(search)))
+
+    if teacher_id > 0:
+        selected_teacher = Teacher.query.get_or_404(teacher_id)
+        pagination = pagination.filter(Group.teacher_id == teacher_id)
+    else:
+        selected_teacher = None
+
+    pagination = pagination.paginate(page, per_page=20, error_out=False)
+    teachers = Teacher.query.order_by(Teacher.fio).all()
+    return render_template('structure/groups_list.html', pagination=pagination, items=pagination.items,
+                           selected_teacher=selected_teacher, search=search, teachers=teachers)
 
 
 @structure.route('/parents')
